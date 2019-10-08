99452cb60618cdd81768474dab4e86ad6a094f94
Kenneth Nguyen <n.kennethcs@gmail.com>
2019-10-07 20:31:04 -0700

    Change security meta tag back to upgrade-insecure-requests and add weather result spacing

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,22 +4,22 @@ const Weather = props => (
     <div className="weather__info">
         {
             props.city && props.country && <p className="weather__key">Location:
-                <span className="weather__value">{props.city}, {props.country}</span>
+                <span className="weather__value"> {props.city}, {props.country}</span>
             </p>
         }
         {
             props.temperature && <p className="weather__key">Temperature: 
-                <span className="weather__value">{props.temperature} °C | {props.temperature * 9/5 + 32} °F</span>
+                <span className="weather__value"> {props.temperature} °C | {(props.temperature * 9/5 + 32).toFixed(2)} °F</span>
             </p>
         }
         {
             props.humidity && <p className="weather__key">Humidity: 
-                <span className="weather__value">{props.humidity}%</span>
+                <span className="weather__value"> {props.humidity}%</span>
             </p>
         }
         {
             props.description && <p className="weather__key">Description:
-                <span className="weather__value">{props.description}</span>
+                <span className="weather__value"> {props.description}</span>
             </p>
         }
         {
